5921e45c70734d051bf13448bd62321dcd8072c9
Jan Wawszczak <janekw23@gmail.com>
2019-12-05 17:08:59 +0100

    Reflect function works (now) (probably

--- src/origami.ml
+++ src/origami.ml
@@ -4,16 +4,24 @@ type point = float * float
 let first  (x, _) = x
 and second (_, y) = y
 
+(* Dodowanaie, odejmowanie punktów oraz mnożenie punktu przez skalar *)
+let add_points (ax, ay) (bx, by) = (ax +. bx, ay +. by)
+and sub_points (ax, ay) (bx, by) = (ax -. bx, ay -. by)
+and mul_point (s : float) (x, y) = (s *. x, s *. y)
+
 type kartka = point -> int
 
 (* Iloczyn skalarny wektorów [a] i [b] *)
 let dot (a : point) (b : point) =
-	(first a) *. (first b) +. (second a) *. (second b) 
+	(first a) *. (first b) +. (second a) *. (second b)
 
-(* Odbija punkt [x] względem prostej [l] (rozumianą jako wektor z (0, 0) do l) *)
-let reflect (p : point) (l : point) =
+(* Odbija punkt [x] względem prostej przechodzącej przez [l_orig] oraz [-_end] *)
+let reflect (v : point) (l_orig, l_end) =
+	assert (l_orig <> l_end);
+	let l = sub_points l_end l_orig
+	and p = sub_points v l_orig in
 	let factor = 2. *. (dot p l) /. (dot l l)
-	in (factor *. (first l) -. (first p), factor *. (second l) -. (second p)) 
+	in add_points l_orig (sub_points (mul_point factor l) p)
 
 let prostokat (a : point) (b : point) =
 	function k -> 0
