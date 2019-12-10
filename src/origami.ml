9e197d0f654d266a1bd06b94490138be77bfaa50
Jan Wawszczak <janekw23@gmail.com>
2019-12-10 10:34:18 +0100

    Small tweaks

--- src/origami.ml
+++ src/origami.ml
@@ -1,9 +1,10 @@
-(* TODO: Check possible need of epsilon *)
 let eps = 0.000000001
 let fabs (x : float) = if x < 0. then -. x else x
 
 (* Punkt na płaszczyźnie *)
 type point = float * float
+
+(* Zwraca, ile razy szpilka wbita w kartkę w danym punkcie przebije ją *)
 type kartka = point -> int
 
 (* Pierwsza albo druga składowa punktu *)
@@ -21,7 +22,6 @@ let dot (a : point) (b : point) =
 
 (* Odbija punkt [x] względem prostej przechodzącej przez [l_orig] oraz [l_end] *)
 let reflect (v : point) (l_orig, l_end) =
-	assert (l_orig <> l_end);
 	let l = sub_points l_end l_orig
 	and p = sub_points v l_orig in
 	let factor = 2. *. (dot p l) /. (dot l l)
@@ -32,7 +32,6 @@ let reflect (v : point) (l_orig, l_end) =
 (* Zwraca  0, jeśli [v] leży na [l] 							   *)
 (* Zwraca -1, jeśli [v] leży na prawo [l]						   *)
 let side (v : point) (l_orig, l_end) =
-	assert (l_orig <> l_end);
 	let p = sub_points v l_orig
 	and l = sub_points l_end l_orig in
 	let cross = (first p) *. (second l) -. (second p) *. (first l)
@@ -41,15 +40,21 @@ let side (v : point) (l_orig, l_end) =
 		else if cross < 0. then 1
 		else -1
 
+(* Tworzy prostokątną niezgiętą kartkę *)
 let prostokat (ax, ay) (bx, by) : kartka = function (px, py) ->
-		if px >= ax -. eps && px <= bx +. eps && py >= ay -. eps && py <= by +. eps then 1 else 0	
+		if px >= ax -. eps && px <= bx +. eps &&
+		   py >= ay -. eps && py <= by +. eps
+		   then 1 else 0	
 
+(* Tworzy okrągłą niezgiętą kartkę *)
 let kolko (s : point) (r : float) =
-	let mag (k : point) = sqrt (dot k k) in
-	function (p : point) -> if (mag (sub_points p s) <= r +. eps) then 1 else 0
+	let mag (kx, ky) = sqrt (kx *. kx +. ky *. ky) in
+	function (p : point) ->
+		if mag (sub_points p s) <= r +. eps
+		then 1 else 0
 
+(* Składa kartkę [k] wzdłuż wektora z [a] do [b] *)
 let zloz (a : point) (b : point) (k : kartka) : kartka =
-	assert (a <> b);
 	let fold = (a, b) in
 	function (p : point) ->
 		let sid = side p fold in
@@ -57,5 +62,6 @@ let zloz (a : point) (b : point) (k : kartka) : kartka =
 		else if sid = 0 then k p
 		else 0 
 
+(* Wykonuje serię złożeń [lst] na kartce [k] *)
 let skladaj (lst : (point * point) list) (k : kartka) =
 	List.fold_left (fun x -> function (a, b) -> zloz a b x) k lst
